c9ecf544baf8de6949f67b75627ec4537ad766a5
Seongho Jang <jangsung0212@naver.com>
2022-09-19 22:01:25 +0900

    9.19

---
+++ src/study/ch1.py
@@ -0,0 +1,8 @@
+from math import pi
+print("1.1 도전문제")
+print(4*3*2*1)
+print(1/2)
+print(300*100)
+print(423+1234)
+print((1/100)*1234)
+print(pi*12*12)
--- src/파이썬코드(py)/Ch01/lab_1_2.py
+++ src/파이썬코드(py)/Ch01/lab_1_2.py
@@ -2,14 +2,20 @@
 # 따라하며 배우는 파이썬과 데이터과학(생능출판사 2020)
 # LAB 1-2 터틀 그래픽으로 삼각형을 그려 보자, 37쪽
 #
+from itertools import repeat
 import turtle
 t = turtle.Turtle()
 t.shape("turtle")
 
-t.forward(100)
-t.left(120)
-t.forward(100)
-t.left(120)
-t.forward(100)
+t.forward(60)
+t.left(144)
+t.forward(60)
+t.left(144)
+t.forward(60)
+t.left(144)
+t.forward(60)
+t.left(144)
+t.forward(60)
+   
 
 turtle.done()
