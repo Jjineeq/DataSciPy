5c3506de17bcefcfed37afea7049fd3a50a460ce
Seongho Jang <jangsung0212@naver.com>
2022-09-20 22:10:21 +0900

    9.20ver3

--- src/study/ch1.py
+++ src/study/ch1.py
@@ -1,8 +1,68 @@
-from math import pi
-print("1.1 도전문제")
-print(4*3*2*1)
-print(1/2)
-print(300*100)
-print(423+1234)
-print((1/100)*1234)
-print(pi*12*12)
+import turtle
+# 1.2 (1)
+t = turtle.Turtle()
+t.shape('turtle')
+t.forward(120) # 120pixel만큼 전진
+t.left(120) # 120도 회전
+t.forward(120)
+t.left(120)
+t.forward(120)
+
+t.left(30) #이어서 그리기 위한 회전
+
+#1.2 (2)
+t.forward(120)
+t.left(90)
+t.forward(120)
+t.left(90)
+t.forward(120)
+t.left(90)
+t.forward(120)
+
+turtle.penup()
+t.right(30) #이어서 그리기 위한 회전
+t.forward(100)
+turtle.pendown()
+
+#1.2 (3)
+t.forward(50)
+t.left(60)
+t.forward(50)
+t.left(60)
+t.forward(50)
+t.left(60)
+t.forward(50)
+t.left(60)
+t.forward(50)
+t.left(60)
+t.forward(50)
+
+turtle.penup()
+t.left(30) # 이어서 그리기 위한 회전
+t.forward(200) # 가시성을 위해 일부 떨어짐
+turtle.pendown()
+
+#1.2 (4)
+t.forward(50)
+t.left(36)
+t.forward(50)
+t.left(36)
+t.forward(50)
+t.left(36)
+t.forward(50)
+t.left(36)
+t.forward(50)
+t.left(36)
+t.forward(50)
+t.left(36)
+t.forward(50)
+t.left(36)
+t.forward(50)
+t.left(36)
+t.forward(50)
+t.left(36)
+t.forward(50)
+t.left(36)
+
+
+turtle.exitonclick() # turtle 종료
---
+++ src/study/ch2.py
@@ -0,0 +1,15 @@
+import turtle
+t = turtle.Turtle()
+t.shape('turtle')
+
+t.circle(20)
+t.circle(40)
+t.circle(50)
+
+t.left(90)
+
+t.circle(20)
+t.circle(40)
+t.circle(50)
+
+turtle.exitonclick()
