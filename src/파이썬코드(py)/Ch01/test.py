ec9c2cf361d75546a9a73f997e74506380b31a64
JJINEEQ <jangsung0212@naver.com>
2022-10-29 01:31:23 +0900

    10.29

--- src/파이썬코드(py)/Ch01/test.py
+++ src/파이썬코드(py)/Ch01/test.py
@@ -1,8 +1,12 @@
 num=[[1,2],[3,4],[5,6]]
 a=[]
 
-for i in range(3):
-    f = num[i]
-    for j in range(2):
-        a.append(f[j])
-print(a)
+#for i in range(3):
+#    f = num[i]
+#    for j in range(2):
+#        a.append(f[j])
+#print(a)
+
+import math
+
+print(math.exp(10)/math.exp(3))
